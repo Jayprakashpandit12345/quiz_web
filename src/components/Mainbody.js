b471ee8fbff770902e0b84c3b72afaf1dda9ac8e
Jayprakashpandit12345 <jayprakashpandit2001@gmail.com>
2023-03-21 22:24:56 +0530

    third push

---
+++ src/components/Mainbody.js
@@ -0,0 +1,11 @@
+function Mainbody(){
+    return(
+        <div className="mb">
+            <p>
+                    vaehvlubjbiuszbjdjk zekcawfncifanlhscfoinhciofio
+            </p>
+        </div>
+    )
+}
+
+export default Mainbody
---
+++ src/components/Navbar1.js
@@ -0,0 +1,15 @@
+import './Navbar1.css';
+
+function Navbar1(){
+    return(
+        <div className='gh'>
+<h1>
+    eurhihvnevjnkdnv.
+</h1>
+
+        </div>
+    );
+
+}
+
+export default Navbar1
